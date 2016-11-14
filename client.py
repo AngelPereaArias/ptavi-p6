6b639eab1e8bbbb351d8725a98235e09c9cb9f5f
Angel Perea Arias <aperea@gamma03.aulas.gsyc.urjc.es>
2016-11-14 10:55:07 +0100

    sys.argv implemented

--- client.py
+++ client.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Programa cliente que abre un socket a un servidor
-"""
+"""Programa cliente que abre un socket a un servidor."""
 
 import socket
+import sys
+import os
 
 # Cliente UDP simple.
+os.system("clear")
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+try:
+    METHOD = sys.argv[1]
+    Aux = sys.argv[2]
+    Aux_Line = Aux[:Aux.find(":")]
+    RECEIVER = Aux[:Aux.find("@")]
+    IP = Aux[Aux.find("@")+1:Aux.rfind(":")]
+    PORT = int(Aux[Aux.rfind(":")+1:])
+
+except:
+    sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
 
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+if METHOD == ("INVITE"):
+    LINE = "INVITE " + Aux_Line + " SIP/2.0"
+elif METHOD == ("ACK"):
+    LINE = "ACK " + Aux_Line + " SIP/2.0"
+elif METHOD == ("BYE"):
+    LINE = "BYE " + Aux_Line + " SIP/2.0"
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((IP, PORT))
 
 print("Enviando: " + LINE)
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
