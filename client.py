2d6f2ea1ecea440bd2623a64894e1a079ae497cc
Angel <angel.pereaarias@gmail.com>
2016-11-20 20:27:56 +0100

    Fixed problems everithig works.

--- client.py
+++ client.py
@@ -22,11 +22,9 @@ except:
 
 # Contenido que vamos a enviar
 if METHOD == ("INVITE"):
-    LINE = "INVITE " + Aux_Line + " SIP/2.0"
-elif METHOD == ("ACK"):
-    LINE = "ACK " + Aux_Line + " SIP/2.0" """se envia al recibir los try/rng/ok"""
+    LINE = "INVITE sip:" + Aux_Line + " SIP/2.0"
 elif METHOD == ("BYE"):
-    LINE = "BYE " + Aux_Line + " SIP/2.0" """se llama al acabar el streaming de audio desde el server"""
+    LINE = "BYE sip:" + Aux_Line + " SIP/2.0"
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -39,6 +37,15 @@ data = my_socket.recv(1024)
 
 print('Recibido -- ', data.decode('utf-8'))
 print("Terminando socket...")
+
+lista_aux = data.decode('utf-8')
+lista = lista_aux.split('\r\n\r\n')[0:-1]
+
+if lista == ['SIP/2.0 100 Trying', 'SIP/2.0 180 Ring', 'SIP/2.0 200 OK']:
+    LINE = "ACK sip:" + Aux_Line + " SIP/2.0"
+    print("Enviando: " + LINE)
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    data = my_socket.recv(1024)
 
 # Cerramos todo
 my_socket.close()
