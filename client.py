28d14dc8ac5a925025ada4605e7c058d4169e8d4
Angel Perea Arias <aperea@gamma03.aulas.gsyc.urjc.es>
2016-11-21 10:02:36 +0100

    Finished code.

--- client.py
+++ client.py
@@ -25,15 +25,19 @@ if METHOD == ("INVITE"):
     LINE = "INVITE sip:" + Aux_Line + " SIP/2.0"
 elif METHOD == ("BYE"):
     LINE = "BYE sip:" + Aux_Line + " SIP/2.0"
+else:
+    LINE = METHOD
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((IP, PORT))
 
+
 print("Enviando: " + LINE)
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 data = my_socket.recv(1024)
+
 
 print('Recibido -- ', data.decode('utf-8'))
 print("Terminando socket...")
